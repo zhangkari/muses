acd5b65d359bfeba2ecdfb42b1f2a63d43e19a6c
Karic <zhangkaric@gmail.com>
2021-02-01 16:25:40 +0800

    Release lib Muses v0.0.1

--- app/src/main/java/com/class100/yunshixun/MainActivity.kt
+++ app/src/main/java/com/class100/yunshixun/MainActivity.kt
@@ -1,28 +1,11 @@
 package com.class100.yunshixun
 
-import android.view.View
-import com.class100.atropos.generic.AtFreqClick
-import com.class100.oceanides.OcActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : OcActivity() {
-    private val multiClick = lazy {
-        AtFreqClick(5, 1000)
-    }
-
-    override fun getContentLayout(): Int {
-        return R.layout.activity_main
-    }
-
-    override fun init() {
-        super.init()
-        setListener()
-    }
-
-    private fun setListener() {
-        findViewById<View>(android.R.id.content).setOnClickListener { _ ->
-            multiClick.value.onClick {
-                DevOpsActivity.launch(this)
-            }
-        }
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
